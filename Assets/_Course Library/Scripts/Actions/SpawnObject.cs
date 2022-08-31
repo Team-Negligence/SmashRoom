5ee242d11b73f29360624a771ed7fa79cca97709
Dannie Cai <s4647457@labs.eait.uq.edu.au>
2022-08-31 12:23:50 +1000

    Added script to spawn items. The script is only attached to the container.

--- Assets/_Course Library/Scripts/Actions/SpawnObject.cs
+++ Assets/_Course Library/Scripts/Actions/SpawnObject.cs
@@ -9,16 +9,46 @@ public class SpawnObject : MonoBehaviour
     public GameObject originalObject = null;
 
     [Tooltip("The transform where the object is spanwed")]
-    public Transform spawnPosition = null;
+    public Transform parent = null;
 
-    public void Spawn()
+    public int numberOfItems;
+
+    public float x;
+    public float y;
+    public float z;
+
+    public void Start()
     {
-        Instantiate(originalObject, spawnPosition.position, spawnPosition.rotation);
+        // Test code to make sure that it works.
+        /*for (int i  = 0; i < numberOfItems; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            Instantiate(originalObject, position, parent.rotation, parent); 
+            Debug.Log("Start");
+        }
+        */
     }
 
     private void OnValidate()
     {
-        if (!spawnPosition)
-            spawnPosition = transform;
+        if (!parent)
+            parent = transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Button.PrimaryThumbStick)
+        {
+            Spawn();
+            Debug.Log("Input is good");
+        }
+        
+    }
+
+    private void Spawn()
+    {
+        // We don't randomise the y value because we don't want the can to float.
+        Vector3 position = new Vector3(Random.Range(x - 0.2f, x + 0.2f), y, Random.Range(z - 0.2f, z + 0.2f));
+        Instantiate(originalObject, position, parent.rotation, parent); 
     }
 }
