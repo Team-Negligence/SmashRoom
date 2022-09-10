77f78b5057a8f3859ddbbeebb5dd813379bffbc3
RainbowGamer707 <88018775+RainbowGamer707@users.noreply.github.com>
2022-09-10 17:44:25 +1000

    Fixed Pointer Issues
    Menu should now work *mostly* as intended

---
+++ Assets/Scripts/LineRendererSettings.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineRendererSettings : MonoBehaviour
+{
+    //DEclare a LineRenderer to store the component attached to the GameObject
+    [SerializeField] LineRenderer rend;
+
+    //Settings for the LineRenderer are stored as a Vector3 array of points. Set up a V3 array to intialize in Start
+    Vector3[] points;
+
+    public LayerMask layerMask;
+    public Button btn;
+
+    public bool AlignLineRenderer(LineRenderer rend)
+    {
+        Ray ray;
+        ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool hitBtn = false;
+
+        if (Physics.Raycast(ray, out hit, layerMask))
+        {
+            points[1] = transform.forward + new Vector3(0, 0, hit.distance);
+            btn = hit.collider.gameObject.GetComponent<Button>();
+            hitBtn = true;
+        }
+        else
+        {
+            points[1] = transform.forward + new Vector3(0, 0, 20);
+            hitBtn = false;
+        }
+
+        rend.SetPositions(points);
+        return hitBtn;
+    }
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        //get the LineRenderer attached to the GameObject
+        rend = gameObject.GetComponent<LineRenderer>();
+
+        points = new Vector3[2];
+
+        points[0] = Vector3.zero;
+
+        points[1] = transform.position + new Vector3(0, 0, 20);
+
+        rend.SetPositions(points);
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AlignLineRenderer(rend);
+
+        if (AlignLineRenderer(rend) && Input.GetAxis("Submit") > 0)
+        {
+            btn.onClick.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI_Interactor.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Interactor : MonoBehaviour
+{
+    LineRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<LineRenderer>();
+    }
+
+    public LayerMask layerMask;
+    public Button btn;
+
+    public bool detectHit(LineRenderer rend)
+    {
+        bool isHit = false;
+
+        Ray ray;
+        ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, layerMask))
+        {
+            btn = hit.collider.gameObject.GetComponent<Button>();
+            isHit = true;
+        } else
+        {
+            isHit = false;
+        }
+
+        return isHit;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (detectHit(rend) && Input.GetAxis("Submit") > 0)
+        {
+            btn.onClick.Invoke();
+        }
+    }
+}
